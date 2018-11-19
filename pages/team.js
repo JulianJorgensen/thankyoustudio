c4b79a27a9476db45acdce9fa2e0dfa519367cbf
Julian Jorgensen <jj@julianjorgensen.com>
2018-11-19 11:05:35 +0100

    Fix build error

--- pages/team.js
+++ pages/team.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import styles from 'utils/styles';
 
 const Wrapper = styled.div`
   ${styles.defaultWrapper};
