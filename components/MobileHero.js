1344b0b68c01ee4a8bd5eebc0d82d5c835195ff0
Julian Jorgensen <jj@julianjorgensen.com>
2018-11-21 20:54:34 +0100

    Lazyload instagram feed

--- components/MobileHero.js
+++ components/MobileHero.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   height: 100vw;
+  width: 100%;
   background-color: red;
 `
 
 export default () => (
   <Wrapper>
+    <h2>Ismobile!!</h2>
   </Wrapper>
 );
