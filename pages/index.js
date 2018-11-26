93b688df0c9723b19b307c63fdaf8c7bcd7391d7
Julian Jorgensen <jj@julianjorgensen.com>
2018-11-26 18:47:19 +0100

    mobile optimize landing hero

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ const Lead = styled.div`
 
 export default (props) => (
   <Case isPrimaryPage whiteContent {...props}>
-    <MobileHero isLanding isMobile={true} />
+    <MobileHero isLanding isMobile={props.isMobile} />
     <Section>
       <IdentifySimplifyAmplify />
     </Section>
